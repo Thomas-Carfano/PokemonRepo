b90339b0884c2322ac24dc8ff6218e9ab4045fe1
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-07-14 13:33:24 -0700

    create second page with picture and abilities from API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const getUl = document.querySelector('ul');
 const getDiv = document.querySelector('div');
-
+ 
 const getPokeData = async () => {
     try{
         const pokeAPI = await fetch('https://pokeapi.co/api/v2/pokemon');
@@ -19,27 +19,45 @@ const renderAllPoke = async () => {
     const nameList = datalist.map((arr) => {
         const a1 = arr.url.split('/');
         const a2 = a1[6];
-        return `<a href="" data-link-num="${a2}"><li>${arr.name}</li></a>`
+        return `<a style="text-decoration: none" href="" data-link-num="${a2}"><li>${arr.name}</li></a>`;
     }).join('');
     getUl.innerHTML = `${nameList}`;    
 };
 
 
 const displayPokeDetails = async () => {
-    const getA = document.querySelectorAll('a')
-    console.log("TEST")
-    console.log(getA)
+    const getA = document.querySelectorAll('a');
 
     for(let i = 0; i < getA.length; i++) {
-        console.log(getA[i]);
         getA[i].addEventListener('click', async (e) => {
             e.preventDefault();
-            const num = getA[i].dataset.linkNum
+
+            const num = getA[i].dataset.linkNum;
             const indPoke2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
-            const pokeAPIResponse2 = await indPoke2.json();
-            getDiv.innerText = JSON.stringify(pokeAPIResponse2, null, 2)
+            const pokeResponse = await indPoke2.json();
+
+            getUl.innerHTML = "";
+            const abilityToPrint = pokeResponse.abilities[0].ability.name;
+            getUl.innerHTML = `<h2> Ability: ${abilityToPrint} </h2>`
+
+            const img = document.createElement('img');
+            img.src = pokeResponse.sprites.front_default;
+            img.style.height = "200px";
+
+            const backButton = document.createElement('button');
+            backButton.innerHTML = "Go Back";
+            backButton.addEventListener('click', backButtonHandler);
+
+            getDiv.appendChild(img);
+            getDiv.appendChild(backButton);
         })
     }
+};
+
+const backButtonHandler = async () => {
+    getUl.innerHTML = "";
+    getDiv.innerHTML = "";
+    await init();
 };
 
 const init = async () => {
